f561307c835748682bc5ad5fdc328306845bf49a
Simeon M. Smith <ssmith@wombatweb.us>
2016-06-29 18:05:59 -0700

    Sudo code created with comments.

---
+++ Madlib/main.py
@@ -0,0 +1,51 @@
+#------------------------------REQUIREMENTS---------------------------------------
+#3 Strings
+#3 Numbers
+#One Array
+#One Dictionary
+#2 Math Operators
+#Two Conditional Statements with one logical operator
+#One function with return value and 2 paramenters
+#One Loop
+
+#COMMENT WELL
+#ORIGINAL
+#ALL ELEMENTS USED
+#6 COMMITS
+#NO ERRORS
+#PRINT OUT STORY
+
+
+#3 Main template strings that makeup the story, one the story, one about getting a drink, one about not being able to afford a drink.
+
+#An array of British insults to insert into the template strings.
+
+#A dictionary containing drinks and their prices
+
+#A prompt asking for the users drinks budget.
+
+#A prompt asking for a their favorite liquor.
+
+#Ask the user for their favorite letter.
+
+#A function that accepts the user prompts and returns the story.
+    #Drink Variable
+    #Insult Variable
+    #Number of the letter var = num of letter %10
+
+    #Based on their favorite liquor find out if they can afford a drink.
+        #If yes then define the drink var as that drink.
+    #Else
+        #If no then define the drink var as false.
+    
+    #Using a loop find the insult using the number from the variable above.
+        #Define the insult var as the insult.
+
+    #if drink var is true
+        #return the story inserting the british insult and the drink line inserting the drink
+    #else
+        #return the story inserting the birtish insult and the line about not being able to affor a drink
+
+#story = function with the user prompts as params
+
+#print story
