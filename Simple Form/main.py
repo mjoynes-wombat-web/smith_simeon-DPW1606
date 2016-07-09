d43801c14c5a343b101f2797b72cea2c87b8f24a
ssmith-wombatweb <ssmith@wombatweb.us>
2016-07-09 16:29:47 -0700

    Form Design Setup - simple-form

--- Simple Form/main.py
+++ Simple Form/main.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Simeon Smith
+DPW1607
+07-09-16
+Simple Form
+'''
+
+#REQUIREMENTS
+    #Collect 5 pieces of information.
+        #One text based
+        #One radio button
+        #One select item
+    #Organize HTML elements in page class.
+    #Use GET method to deliver info gathered.
+    #List information after GET.
+    #Create a polished and professional design.
+    #Comment code.
+    #Make 6 commits to GitHub.
+    #No global variables.
+    #No errors.
+    #No frameworks.
+    
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
