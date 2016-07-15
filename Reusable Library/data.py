575783ebe9066ff49ed40edb574b45e77231b9e1
ssmith-wombatweb <ssmith@wombatweb.us>
2016-07-15 15:30:02 -0700

    Reusable Library: Created product data class.

---
+++ Reusable Library/data.py
@@ -0,0 +1,9 @@
+class Product(object):
+    def __init__(self):
+        self.__name = ''
+        self.__brand = ''
+        self.__price = ''
+        self.__weight = 0
+        self.__weight_unit = ''
+
+    
