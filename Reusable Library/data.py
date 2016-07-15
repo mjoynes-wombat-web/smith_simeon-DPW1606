b20f9b3a4e4c0119ea5d9e291442837d4e945d89
ssmith-wombatweb <ssmith@wombatweb.us>
2016-07-15 15:41:06 -0700

    Added getters and setters for the Product data.

--- Reusable Library/data.py
+++ Reusable Library/data.py
@@ -6,4 +6,42 @@ class Product(object):
         self.__weight = 0
         self.__weight_unit = ''
 
-    
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, p_name):
+        self.__name = p_name
+
+    @property
+    def brand(self):
+        return self.__brand
+
+    @brand.setter
+    def brand(self, p_brand):
+        self.__brand = p_brand
+
+    @property
+    def price(self):
+        return "$" + str(self.__price)
+
+    @price.setter
+    def price(self, p_price):
+        self.__price = p_price
+
+    @property
+    def weight(self):
+        return str(self.__weight) + ' ' + self.__weight_unit
+
+    @weight.setter
+    def weight(self, p_weight):
+        self.__weight = p_weight
+
+    @property
+    def weight_unit(self):
+        pass
+
+    @weight_unit.setter
+    def weight_unit(self, p_weight_unit):
+        self.__weight_unit = p_weight_unit
