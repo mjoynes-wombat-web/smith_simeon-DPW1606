07613d0acabdd60a71a004bc83fed830ad3f63c9
ssmith-wombatweb <ssmith@wombatweb.us>
2016-07-22 17:16:55 -0700

    DS - Added necessary files and comments for reqs.

--- Dynamic Site/main.py
+++ Dynamic Site/main.py
@@ -1,3 +1,43 @@
+'''
+Simeon Smith
+DPW1607
+07-22-16
+Dynamic Site
+'''
+
+#REQUIREMENTS
+#5 pages that are generated based on a query refence to the page data in the URL.
+#A main "home" page.
+#The pages are accessed from link "buttons".
+#Use some of the data in a calculation.
+
+    #main.py
+    #MainHandler class
+        #Contains instances of data and page class.
+        #Transfer the information between the data and page classes.
+        #Only place self.request should be.
+
+    #data.py
+    #DataObject class
+    #5 data objects created from the above class
+
+    #pages.py
+    #Page class
+        #Contains general HTML templates
+    #ContentPage class
+        #Inherits from the page class
+        #Adds custom templates specific to this application
+
+    #lib.py
+        #Optional for extra credit.
+
+#Professionaly Designed
+#Comment code in detail
+#Commit 20 times.
+#No frameworks.
+#No global variables
+#NO ERRORS
+#Unique and Original
 
 import webapp2
 
