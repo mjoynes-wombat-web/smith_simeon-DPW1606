a740b0ba12d28dcc3a11623160f1804aa65b252a
ssmith-wombatweb <ssmith@wombatweb.us>
2016-07-26 20:02:26 -0700

    DS - Completed design for main page.

--- Dynamic Site/main.py
+++ Dynamic Site/main.py
@@ -9,27 +9,27 @@ Dynamic Site
 #5 pages that are generated based on a query refence to the page data in the URL.
 #A main "home" page.
 #The pages are accessed from link "buttons".
-#Use some of the data in a calculation.
+#!Use some of the data in a calculation.
 
     #main.py
-    #MainHandler class
-        #Contains instances of data and page class.
-        #Transfer the information between the data and page classes.
-        #Only place self.request should be.
+        #MainHandler class
+            #Contains instances of data and page class.
+            #Transfer the information between the data and page classes.
+            #Only place self.request should be.
 
-    #data.py
-    #DataObject class
-    #5 data objects created from the above class
+    #!data.py
+        #!DataObject class
+        #!5 data objects created from the above class
 
     #pages.py
-    #Page class
-        #Contains general HTML templates
-    #ContentPage class
-        #Inherits from the page class
-        #Adds custom templates specific to this application
+        #Page class
+            #Contains general HTML templates
+        #ContentPage class
+            #Inherits from the page class
+            #Adds custom templates specific to this application
 
-    #lib.py
-        #Optional for extra credit.
+    #!lib.py
+        #!Optional for extra credit.
 
 #Professionaly Designed
 #Comment code in detail
@@ -46,7 +46,6 @@ import datetime
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
 
         contacts = contactLib()
 
@@ -69,6 +68,8 @@ class MainHandler(webapp2.RequestHandler):
         c5 = Contact()
         c5.add_contact("Maria", "Joynes", 12738539238, "Coldwell Banker", "Marketing Director", "04/22/2010", "09/12/2011")
         contacts.add_contact(c5)
+
+        self.response.write('Hello world!')
 
         
 
